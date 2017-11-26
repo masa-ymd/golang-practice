94c11916e0f70a4b046550b58d26a423318747eb
masa-ymd <masa.ymd.0703@gmail.com>
2017-11-27 00:32:46 +0900

    add commenton echo1

--- ch01/echo1.go
+++ ch01/echo1.go
@@ -6,6 +6,9 @@ import (
 )
 
 func main() {
+    // 変数宣言時に、初期化の値が明示的に指定されない時は
+    // それぞれの方に対するゼロ値が設定される
+    // intは0,stringは""など nilではない
 	var s, sep string
 	sep = " "
 	for i := 1; i < len(os.Args); i++ {
