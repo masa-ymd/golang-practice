1c416e84dd9763d1842231a4cc06cddbc55349e7
masa-ymd <masa.ymd.0703@gmail.com>
2017-11-29 02:44:33 +0900

    add ex1-4

--- ch01/dup2.go
+++ ch01/dup2.go
@@ -7,34 +7,38 @@ import (
 )
 
 func main() {
+	fileCounts := make(map[string]map[string]int)
 	counts := make(map[string]int)
 	files := os.Args[1:]
 	if len(files) == 0 {
-        // Stdin, Stdout, and Stderr are open Files pointing to the standard input, standard output, and standard error file descriptors.
-		countLines(os.Stdin, counts)
+		// Stdin, Stdout, and Stderr are open Files pointing to the standard input, standard output, and standard error file descriptors.
+		countLines(os.Stdin, fileCounts, counts)
 	} else {
 		for _, arg := range files {
 			f, err := os.Open(arg)
 			if err != nil {
-                // 第一引数で出力先を指定可能
+				// 第一引数で出力先を指定可能
 				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
 				continue
 			}
-			countLines(f, counts)
+			countLines(f, fileCounts, counts)
 			f.Close()
 		}
 	}
 
-	for line, n := range counts {
-		if n > 1 {
-			fmt.Printf("%d\t%s\n", n, line)
+	for filename, counts := range fileCounts {
+		for line, n := range counts {
+			if n > 1 {
+				fmt.Printf("%d\t%s\t%s\n", n, line, filename)
+			}
 		}
 	}
 }
 
-func countLines(f *os.File, counts map[string]int) {
+func countLines(f *os.File, fileCounts map[string]map[string]int, counts map[string]int) {
 	input := bufio.NewScanner(f)
 	for input.Scan() {
 		counts[input.Text()]++
+		fileCounts[f.Name()] = counts
 	}
 }
