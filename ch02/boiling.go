988afb8f9f1d9bd521f1b3b776a6e3374d5c0b0a
masa-ymd <masa.ymd.0703@gmail.com>
2017-12-03 00:30:56 +0900

    add ftoc

--- ch02/boiling.go
+++ ch02/boiling.go
@@ -6,8 +6,8 @@ import "fmt"
 const boilingF = 212.0
 
 func main() {
-    // 関数レベルの宣言
-    var f = boilingF
-    var c = (f - 32) * 5 / 9
-    fmt.Printf("boiling point = %gF or %gC\n", f, c)
+	// 関数レベルの宣言
+	var f = boilingF
+	var c = (f - 32) * 5 / 9
+	fmt.Printf("boiling point = %gF or %gC\n", f, c)
 }
