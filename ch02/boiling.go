be9d5d39cf474724924af19ced072e504be1965f
masa-ymd <masa.ymd.0703@gmail.com>
2017-12-03 00:27:02 +0900

    add boiling

---
+++ ch02/boiling.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+// パッケージレベルの宣言
+const boilingF = 212.0
+
+func main() {
+    // 関数レベルの宣言
+    var f = boilingF
+    var c = (f - 32) * 5 / 9
+    fmt.Printf("boiling point = %gF or %gC\n", f, c)
+}
