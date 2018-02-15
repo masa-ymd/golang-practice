0fbe3482d9933089ad77bb200f1ee8848dc51c32
unknown <masa-ymd@nifty.com>
2018-02-16 01:14:18 +0900

    add http

---
+++ ch07/http3/http3.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type dollars float32
+
+func (d dollars) String() string { return fmt.Sprintf("$%.2f", d) }
+
+type database map[string]dollars
+
+func (db database) list(w http.ResponseWriter, req *http.Request) {
+	for item, price := range db {
+		fmt.Fprintf(w, "%s: %s\n", item, price)
+	}
+}
+func (db database) price(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	price, ok := db[item]
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "no such item: %q\n", item)
+		return
+	}
+	fmt.Fprintf(w, "%s\n", price)
+}
+
+func main() {
+	db := database{"shoes": 50, "socks": 5}
+	mux := http.NewServeMux()
+	// list関数をhttp.HandleFunc型に変換
+	mux.Handle("/list", http.HandlerFunc(db.list))
+	// price関数をhttp.HandleFunc型に変換
+	mux.Handle("/price", http.HandlerFunc(db.price))
+	// mux はServeHTTPをもつ
+	log.Fatal(http.ListenAndServe("localhost:8080", mux))
+}
