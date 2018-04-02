2e072d390b62cb77fbe56eb24e8c247b67b04124
unknown <masa-ymd@nifty.com>
2018-04-03 00:55:34 +0900

    add ch11

---
+++ ch11/storage2/storage.go
@@ -0,0 +1,38 @@
+package storage
+
+import (
+	"fmt"
+	"log"
+	"net/smtp"
+)
+
+var usage = make(map[string]int64)
+
+func bytesInUse(username string) int64 { return usage[username] }
+
+const sender = "hogehoge@example.com"
+const password = "password"
+const hostname = "smtp.example.com"
+
+const template = `Warning: you are using %d bytes of strage, %d%% of your quota.`
+
+// テストで別なものに置き換えるため、関数をグローバル変数に保存
+var notifyUser = func(username, msg string) {
+	auth := smtp.PlainAuth("", sender, password, hostname)
+	err := smtp.SendMail(hostname+":587", auth, sender,
+		[]string{username}, []byte(msg))
+	if err != nil {
+		log.Printf("smtp.SendEmail(%s) failed: %s", username, err)
+	}
+}
+
+func CheckQuota(username string) {
+	used := bytesInUse(username)
+	const quota = 1000000000 // 1GB
+	percent := 100 * used / quota
+	if percent < 90 {
+		return // OK
+	}
+	msg := fmt.Sprintf(template, used, percent)
+	notifyUser(username, msg)
+}
